b37f5fbc29c31986a019c45632bbf274f42c70f4
aamimlim <i.aamimlim@gmail.com>
2026-08-17 07:06:03 +0900

    Add files via upload

---
+++ set02.js
@@ -0,0 +1,582 @@
+const quizMetadata = [
+  {
+    text: "[1-4] 빈 칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num:"1.",
+    detail:"한국에서 가장 더운 계절은 ______입니다. 이 때 사람들은 더위를 피하기 위해서 해수욕장이나 계곡으로 놀러 갑니다.",
+    image: "",
+    options: [
+      { text: "가을" },
+      { text: "겨울" },
+      { text: "여름" },
+      { text: "봄" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[1-4] 빈 칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num:"2.",
+    detail:"저는 지금까지 눈을 한번도 못 봤습니다. 한국에서는 눈을 볼수 있으니까 _____ 을 기다리고 있습니다.",
+    image: "",
+    options: [
+      { text: "봄" },
+      { text: "여름" },
+      { text: "가울" },
+      { text: "겨울" }
+    ],
+    correct: 3,
+    points: 2.5
+  },
+ {
+    text: "[1-4] 빈 칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num:"3.",
+    detail:"가: 분명히 집에서 지갑을 가지고 왔는데 지갑이 안 보여 요.\n 나: 다시 한번 잘 ______ 보세요. 가방 안에 있을 거예요.",
+    image: "",
+    options: [
+      { text: "주워" },
+      { text: "배워" },
+      { text: "찾아" },
+      { text: "기다려" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[1-4] 빈 칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num:"4.",
+    detail:"고구마 캐는 일을 하신다고 해서 호미를________ . ",
+    image: "",
+    options: [
+      { text: "뿌렸는데요" },
+      { text: "심었는데요. " },
+      { text: "챙겼는데요. " },
+      { text: "뽑았는데요." }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[5-8] 다음 질문에 답하십시오.",
+    num:"5. 이 표지는 무슨 뜻입니까? ",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_5.webp",
+    options: [
+      { text: "이 곳에서 걸어 다닐 수 없습니다. " },
+      { text: "이 곳에서 운전을 할 수 없습니다. " },
+      { text: "이 곳에서 차량을 탈 수 없습니다. " },
+      { text: "이 곳에서 물체를 옮길 수 없습니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[5-8] 다음 질문에 답하십시오.",
+    num:"6. 이 사람의 직업은 무엇입니까? ",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_6.webp",
+    options: [
+      { text: "기자입니다." },
+      { text: "이대기입니다." },
+      { text: "국민방송입니다." },
+      { text: "사회팀입니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[5-8] 다음 질문에 답하십시오.",
+    num:"7. 다음 안내문에 대한 설명으로 맞는 것은 무엇입니까? ",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_7.webp",
+    options: [
+      { text: "상가 이층으로 이사를 합니다." },
+      { text: "이사 다음 날에 문을 닫습니다." },
+      { text: "오성아파트 상가에서 다른 곳으로 옮깁니다." },
+      { text: "유월 삼일에는 우리마트를 이용할 없습니다." }
+    ],
+    correct: 3,
+    points: 2.5
+  },
+ {
+    text: "[5-8] 다음 질문에 답하십시오.",
+    num:"8. 외국인 관광객의 서울 시내 방문지에 대한 설명으로 맞는 것은 무엇입니까? ",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_8.webp",
+    options: [
+      { text: "관광객의 대부분이 인사동을 방문합니다. " },
+      { text: "면세점을 방문하는 관광객이 가장 적습니다. " },
+      { text: "4대 고궁을 방문한 사람들이 N서울타워를 방문합니다. " },
+      { text: "명동은 외국인 관광객에게 가장 인기가 많은 곳입니다." }
+    ],
+    correct: 3,
+    points: 2.5
+  },
+ {
+    text: "[9-10] 빈칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num:"9.",
+    detail:"새 운동화를 샀는데 집에 와서 신어 보니 좀 작았습니다. 그래서 가게 에 다시 가서 운동화를 더 큰 사이즈로_________ . ",
+    image: "",
+    options: [
+      { text: "포장했습니다." },
+      { text: "환급했습니다." },
+      { text: "환불했습니다." },
+      { text: "교환했습니다." }
+    ],
+    correct: 3,
+    points: 2.5
+  },
+ {
+    text: "[9-10] 빈칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num:"10.",
+    detail:"저는 버스를 타고 회사에 갑니다. 아침마다 버스 정류장은 _______ 사람들이 복잡니다.",
+    image: "",
+    options: [
+      { text: "출근하려고" },
+      { text: "출입하거든" },
+      { text: "출근하거든" },
+      { text: "출입하려는" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[11-12] 다음 글을 읽고 무엇에 대한 글인지 고르십시오.",
+    num:"11.",
+    detail:"오전에 골판지 자르는 작업을 끝내고 오후부터 접고 붙이는 작업을 하고 있습니다. 지난주에 주문한 골판지가 방금 도착해서 작업에는 문제가 없을 것입니다. 6시까지는 주문 받은 상자를 모두 완성할 수 있을 것 같습니다.",
+    image: "",
+    options: [
+      { text: "작업 장소" },
+      { text: "작업상황" },
+      { text: "작업시간" },
+      { text: "작업목적" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+ {
+    text: "[11-12] 다음 글을 읽고 무엇에 대한 글인지 고르십시오.",
+    num:"12.",
+    detail:"직장에서는 단정하고 깔끔하게 옷을 입어야 합니다. 작업을 할 때는 작업복 의 단추를 잠가야 하고 신발끈도 꼭 매 주는 것이 좋습니다.",
+    image: "",
+    options: [
+      { text: "감사 " },
+      { text: "거절 " },
+      { text: "약속 " },
+      { text: "주의" }
+    ],
+    correct: 3,
+    points: 2.5
+  },
+ {
+    text: "[13-14] 다음 글을 읽고 내용과 같은 것을 고르십시오.",
+    num:"13.",
+    detail:"경산물산은 활기찬 직장 분위기를 만들기 위해서 직원들을 대상으로 다양 한 행사를 마련하고 있습니다. 생일 케이크 선물하기, 동료와 차 한잔, 저 녁이 있는 삶과 같은 캠페인을 통해 직원들이 출근하고 싶은 직장이 되도 록 노력하고 있습니다. 경산물산의 부장 강민호씨는 신바람 나는 직장 분 위기는 결국 회사의 생산성과도 연결된다고 말했습니다. ",
+    image: "",
+    options: [
+      { text: "강민호씨는 경산 물산의 사업주입니다." },
+      { text: "활기찬 직장은 직원들이 만들어 나가는 것입니다." },
+      { text: "경산물산은 좋은 직장 분위기를 만들기위해 노력하고 있습니다." },
+      { text: "좋은 직장 분위기는 생산성은 아무 상관이 없습니다." }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[13-14] 다음 글을 읽고 내용과 같은 것을 고르십시오.",
+    num:"14.",
+    detail:"밀가루는 음식 재료입니다. 그런데 밀가루는 다양한 곳에 사용할 수 있습니다. 포도나 딸기를 씻을 때 밀가루로 씻으면 좋습니다. 그리고 냄새가 나는 그롯에 밀가루를 넣고 하루가 지나면 냄새가 나지 않습니다. 밀가루를 사용하면 프라이팬에 남은 기름도 쉽게 닦을 수 있습니다.",
+    image: "",
+    options: [
+      { text: "밀가루로 할 수 있는 일." },
+      { text: "밀가루로 그롯을 닦는 순서." },
+      { text: "밀가루로 과일을 씻는 방법." },
+      { text: "밀가루로 만들 수 있는 음식." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[15-16] 다음 설명에 알맞은 어휘를 고르십시오.",
+    num:"15.",
+    detail:"근로자가 다니던 회사나 직장에서의 일을 그만두고 떠나는 것입니다.",
+    image: "",
+    options: [
+      { text: "결근" },
+      { text: "야근" },
+      { text: "퇴사" },
+      { text: "입사" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[15-16] 다음 설명에 알맞은 어휘를 고르십시오.",
+    num:"16.",
+    detail:"인력난을 겪고 있는 사업장에 일정한 요건 하에 외국인 근로자를 합법 적으로 고용할 수 있도록 허가해 주는 제도입니다. ",
+    image: "",
+    options: [
+      { text: "외국인력지원센터" },
+      { text: "외국인고용허가제" },
+      { text: "고용노동부" },
+      { text: "산업인력공단" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+ {
+    text: "[17-18] 다음 그림을 보고 맞는 단어나 문장을 고르십시오.",
+    num:"17.",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_17.webp",
+    options: [
+      { text: "자" },
+      { text: "차 " },
+      { text: "조 " },
+      { text: "초" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[17-18] 다음 그림을 보고 맞는 단어나 문장을 고르십시오.",
+    num:"18.",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_18.webp",
+    options: [
+      { text: "공부하고 있습니다." },
+      { text: "가르치고 있습니다." },
+      { text: "전화하고 있습니다." },
+      { text: "상담하고 있습니다." }
+    ],
+    correct: 3,
+    points: 2.5
+  },
+ {
+    text: "[19-20] 다음 질문에 답하십시오.",
+    num:"19. 다음 단어와 관계있는 것은 무엇입니까? ",
+    detail:"독서, 낚시, 등산, 여행",
+    image: "",
+    options: [
+      { text: "작업" },
+      { text: "교육" },
+      { text: "운동" },
+      { text: "취미" }
+    ],
+    correct: 3,
+    points: 2.5
+  },
+ {
+    text: "[19-20] 다음 질문에 답하십시오.",
+    num:"20. 다음 단어와 비슷한 말은 무엇입니까? ",
+    detail:"지급하다",
+    image: "",
+    options: [
+      { text: "신청하다 " },
+      { text: "지불하다" },
+      { text: "소멸하다" },
+      { text: "청구하다" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+ {
+    text: "[21-24] 들은 것을 고르십시오.",
+    num:"21.",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-21.mp3",
+    options: [
+      { text: "고향 " },
+      { text: "고용 " },
+      { text: "고형 " },
+      { text: "고양" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+ {
+    text: "[21-24] 들은 것을 고르십시오.",
+    num:"22.",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-22.mp3",
+    options: [
+      { text: "상처" },
+      { text: "상추" },
+      { text: "상지" },
+      { text: "상자" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[21-24] 들은 것을 고르십시오.",
+    num:"23.",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_23.webp",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-23.mp3",
+    options: [
+      { text: "1" },
+      { text: "2" },
+      { text: "3" },
+      { text: "4" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[21-24] 들은 것을 고르십시오.",
+    num:"24.",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_24.webp",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-24.mp3",
+    options: [
+      { text: "1" },
+      { text: "2" },
+      { text: "3" },
+      { text: "4" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[25-29] 그림을 보고 알맞은 대답을 고르십시오.",
+    num:"25. 이것은 무엇입니까? ",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_25.webp",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-25.mp3",
+    options: [
+      { text: "" },
+      { text: "" },
+      { text: "" },
+      { text: "" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[25-29] 그림을 보고 알맞은 대답을 고르십시오.",
+    num:"26. 여기는 어디입니까? ",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_26.webp",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-26.mp3",
+    options: [
+      { text: "" },
+      { text: "" },
+      { text: "" },
+      { text: "" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[25-29] 그림을 보고 알맞은 대답을 고르십시오.",
+    num:"27. 이 남자는 지금 무엇을 하고 있습니까? ",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_27.webp",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-27.mp3",
+    options: [
+      { text: "" },
+      { text: "" },
+      { text: "" },
+      { text: "" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[25-29] 그림을 보고 알맞은 대답을 고르십시오.",
+    num:"28. 의자가 얼마나 있습니까? ",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_28.webp",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-28.mp3",
+    options: [
+      { text: "" },
+      { text: "" },
+      { text: "" },
+      { text: "" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+ {
+    text: "[25-29] 그림을 보고 알맞은 대답을 고르십시오.",
+    num:"29. 주민센터는 어디에 있습니까? ",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_29.webp",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-29.mp3",
+    options: [
+      { text: "" },
+      { text: "" },
+      { text: "" },
+      { text: "" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[30-33] 다음을 듣고 질문에 알맞은 대답을 고르십시오.",
+    num:"30.",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-30.mp3",
+    options: [
+      { text: "저건 제 신발이에요" },
+      { text: "필리핀으로 여행 가요" },
+      { text: "우리 회사 반장님이에요" },
+      { text: "저기가 우리 집이에요" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[30-33] 다음을 듣고 질문에 알맞은 대답을 고르십시오.",
+    num:"31.",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g4_set4-31.mp3",
+    options: [
+      { text: "여기에 세워주세요." },
+      { text: "같이 운동할래요?" },
+      { text: "에어컨을 켤까요?" },
+      { text: "발목이 좀 아파요. " }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+ {
+    text: "[30-33] 다음을 듣고 질문에 알맞은 대답을 고르십시오.",
+    num:"32.",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-32.mp3",
+    options: [
+      { text: "구호품을 제공해 줘요. " },
+      { text: "기숙사만 해 준다고 해요. " },
+      { text: "숙소는 예약이 끝났어요. " },
+      { text: "식당은 회사 옆에 있어요." }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+ {
+    text: "[30-33] 다음을 듣고 질문에 알맞은 대답을 고르십시오.",
+    num:"33.",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-33.mp3",
+    options: [
+      { text: "네, 장을 봐 오세요. " },
+      { text: "네, 상을 차리세요. " },
+      { text: "아니요, 작년에 했어요." },
+      { text: "아니요, 지금 해 주세요." }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+ {
+    text: "[34-35] 다음을 듣고 이어지는 말을 고르십시오.",
+    num:"34.",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-34.mp3",
+    options: [
+      { text: "네. 잠깐만요." },
+      { text: "지금 통화 중이세요?" },
+      { text: "전화를 잘못 거셨어요." },
+      { text: "실례지만 누구를 찾으세요?" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[34-35] 다음을 듣고 이어지는 말을 고르십시오.",
+    num:"35.",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-35.mp3",
+    options: [
+      { text: "시청역 근처예요." },
+      { text: "공부하고 있어요." },
+      { text: "김영수라고 해요." },
+      { text: "네, 안녕히 가세요." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[36-37] 다음을 듣고 들은 내용과 관계 있는 그림을 고르십시오.",
+    num:"36.",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_36.webp",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-36.mp3",
+    options: [
+      { text: "" },
+      { text: "" },
+      { text: "" },
+      { text: "" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[36-37] 다음을 듣고 들은 내용과 관계 있는 그림을 고르십시오.",
+    num:"37.",
+    detail:"",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2_37.webp",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-37.mp3",
+    options: [
+      { text: "" },
+      { text: "" },
+      { text: "" },
+      { text: "" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+ {
+    text: "[38-40] 다음 이야기를 듣고 질문에 알맞은 대답을 고르십시오.",
+    num:"38. 여자는 남자에게 왜 전화를 했습니까? ",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-38.mp3",
+    options: [
+      { text: "부탁하려고" },
+      { text: "안내하려고" },
+      { text: "조언하려고" },
+      { text: "건의 하려고" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+ {
+    text: "[38-40] 다음 이야기를 듣고 질문에 알맞은 대답을 고르십시오.",
+    num:"39. 여자에 대한 설명으로 맞는 것은 무엇입니까? ",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-39.mp3",
+    options: [
+      { text: "냄새가 심해서 힘들었습니다." },
+      { text: "냄새가 안 나서 마스크를 안 썼습니다." },
+      { text: "페인트 작업은 별로 어렵지 않았습니다." },
+      { text: "지금까지 자주 페인트 작업을 했습니다." }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+ {
+    text: "[38-40] 다음 이야기를 듣고 질문에 알맞은 대답을 고르십시오.",
+    num:"40. 남자는 어떤 일을 하고 있습니까?  ",
+    detail:"",
+    image: "",
+    audio:"https://jettyland.wordpress.com/wp-content/uploads/2025/01/g5_set2-40.mp3",
+    options: [
+      { text: "물건을 주문하는 일 " },
+      { text: "창고를 관리하는 일 " },
+      { text: "상품을 출고하는 일 " },
+      { text: "물건을 소진하는 일" }
+    ],
+    correct: 1,
+    points: 2.5
+  }
+];
