8035d543a2d80d6b459b3ac6c4dda66000a87763
mooyeng <i.mooyang@gmail.com>
2026-08-27 20:02:03 +0900

    add

---
+++ gptz03.js
@@ -0,0 +1,630 @@
+const quizMetadata = [
+
+  {
+    text: "[1~4] 다음 그림을 보고 알맞은 것을 고르십시오.",
+    num: "1.",
+    detail: "",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-01.webp",
+    audio: "",
+    options: [
+      { text: "안전모" },
+      { text: "안전화" },
+      { text: "장갑" },
+      { text: "마스크" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[1~4] 다음 그림을 보고 알맞은 것을 고르십시오.",
+    num: "2.",
+    detail: "",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-02.webp",
+    audio: "",
+    options: [
+      { text: "용접하고 있습니다." },
+      { text: "회의하고 있습니다." },
+      { text: "청소하고 있습니다." },
+      { text: "식사하고 있습니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[1~4] 다음 그림을 보고 알맞은 것을 고르십시오.",
+    num: "3.",
+    detail: "",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-03.webp",
+    audio: "",
+    options: [
+      { text: "은행" },
+      { text: "약국" },
+      { text: "병원" },
+      { text: "우체국" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+
+  {
+    text: "[1~4] 다음 그림을 보고 알맞은 것을 고르십시오.",
+    num: "4.",
+    detail: "",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-02.webp",
+    audio: "",
+    options: [
+      { text: "지게차" },
+      { text: "크레인" },
+      { text: "사다리" },
+      { text: "버스" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[5~6] 다음 중 알맞은 것을 고르십시오.",
+    num: "5.",
+    detail: "저는 매일 회사___ 갑니다.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "에" },
+      { text: "를" },
+      { text: "가" },
+      { text: "와" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[5~6] 다음 중 알맞은 것을 고르십시오.",
+    num: "6.",
+    detail: "어제 비___ 많이 왔습니다.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "가" },
+      { text: "를" },
+      { text: "은" },
+      { text: "도" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[7~10] 다음을 읽고 물음에 답하십시오.",
+    num: "7.",
+    detail: "출입 금지",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-07.webp",
+    audio: "",
+    options: [
+      { text: "들어가도 됩니다." },
+      { text: "들어갈 수 없습니다." },
+      { text: "사진을 찍으십시오." },
+      { text: "회의를 하십시오." }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+
+  {
+    text: "[7~10] 다음을 읽고 물음에 답하십시오.",
+    num: "8.",
+    detail: "안전모를 반드시 착용하십시오.",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-08.webp",
+    audio: "",
+    options: [
+      { text: "안전모가 필요 없습니다." },
+      { text: "안전화를 신으십시오." },
+      { text: "안전모를 써야 합니다." },
+      { text: "작업을 중단하십시오." }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+
+  {
+    text: "[7~10] 다음을 읽고 물음에 답하십시오.",
+    num: "9.",
+    detail: "점심시간 12:00 ~ 13:00",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-09.webp",
+    audio: "",
+    options: [
+      { text: "점심시간은 한 시간입니다." },
+      { text: "점심시간은 두 시간입니다." },
+      { text: "오전 12시에 시작합니다." },
+      { text: "점심시간이 없습니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[7~10] 다음을 읽고 물음에 답하십시오.",
+    num: "10.",
+    detail: "공장 정기 점검으로 인해 7월 20일 작업이 없습니다.",
+    image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-10.webp",
+    audio: "",
+    options: [
+      { text: "7월 20일에 야근합니다." },
+      { text: "7월 20일에 작업합니다." },
+      { text: "7월 20일에 작업이 없습니다." },
+      { text: "공장이 문을 닫지 않습니다." }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+  {
+    text: "[11~16] 빈칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num: "11.",
+    detail: "작업을 시작하기 전에 안전교육을 ______ 합니다.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "받아야" },
+      { text: "받으면" },
+      { text: "받거나" },
+      { text: "받아서" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[11~16] 빈칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num: "12.",
+    detail: "기계가 갑자기 멈춰서 관리자에게 ______.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "보고했습니다" },
+      { text: "출근했습니다" },
+      { text: "운전했습니다" },
+      { text: "도착했습니다" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[11~16] 빈칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num: "13.",
+    detail: "오늘은 몸이 안 좋아서 병원에 ______.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "갔습니다" },
+      { text: "왔습니다" },
+      { text: "만났습니다" },
+      { text: "배웠습니다" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[11~16] 빈칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num: "14.",
+    detail: "물건이 무거우니까 혼자 들지 말고 같이 ______.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "옮기세요" },
+      { text: "쉬세요" },
+      { text: "주무세요" },
+      { text: "기다리세요" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[11~16] 빈칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num: "15.",
+    detail: "비가 많이 와서 우산을 ______ 갔습니다.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "쓰고" },
+      { text: "입고" },
+      { text: "신고" },
+      { text: "차고" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[11~16] 빈칸에 들어갈 가장 알맞은 것을 고르십시오.",
+    num: "16.",
+    detail: "퇴근 후에 친구를 만나서 저녁을 ______.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "먹었습니다" },
+      { text: "탔습니다" },
+      { text: "보냈습니다" },
+      { text: "배웠습니다" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "17. 다음 설명에 알맞은 것을 고르십시오.",
+    num: "17.",
+    detail: "공장에서 무거운 물건을 들어 올리거나 옮길 때 사용하는 차량입니다.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "지게차" },
+      { text: "버스" },
+      { text: "택시" },
+      { text: "오토바이" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "18. 다음 글을 읽고 무엇에 대한 내용인지 고르십시오.",
+    num: "18.",
+    detail: "작업장에서는 반드시 안전모와 안전화를 착용해야 하며 위험한 행동을 해서는 안 됩니다.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "급여" },
+      { text: "안전수칙" },
+      { text: "휴가" },
+      { text: "식사시간" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+
+  {
+    text: "[19~20] 다음 글을 읽고 내용과 같은 것을 고르십시오.",
+    num: "19.",
+    detail: "민수 씨는 한국에 온 지 3년이 되었습니다. 처음에는 한국어가 어려웠지만 지금은 회사 동료들과 자연스럽게 대화할 수 있습니다.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "민수 씨는 한국어를 못합니다." },
+      { text: "민수 씨는 한국에 온 지 3개월 되었습니다." },
+      { text: "민수 씨는 동료들과 대화할 수 있습니다." },
+      { text: "민수 씨는 학생입니다." }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+
+  {
+    text: "[19~20] 다음 글을 읽고 내용과 같은 것을 고르십시오.",
+    num: "20.",
+    detail: "다음 주 월요일 오전 9시에 안전교육이 있습니다. 모든 직원은 교육장에 참석해야 합니다.",
+    image: "",
+    audio: "",
+    options: [
+      { text: "안전교육은 오후에 있습니다." },
+      { text: "직원은 교육에 참석해야 합니다." },
+      { text: "안전교육은 취소되었습니다." },
+      { text: "관리자만 교육을 받습니다." }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+
+  {
+    text: "[21~22] 들은 것을 고르십시오.",
+    num: "21.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-21.mp3",
+    options: [
+      { text: "안전모" },
+      { text: "안전화" },
+      { text: "안전벨트" },
+      { text: "장갑" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+
+  {
+    text: "[21~22] 들은 것을 고르십시오.",
+    num: "22.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-22.mp3",
+    options: [
+      { text: "운반하다" },
+      { text: "조립하다" },
+      { text: "포장하다" },
+      { text: "청소하다" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[23~26] 듣고 알맞은 그림을 고르십시오.",
+    num: "23.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-23.mp3",
+    options: [
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-23-1.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-23-2.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-23-3.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-23-4.webp" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+
+  {
+    text: "[23~26] 듣고 알맞은 그림을 고르십시오.",
+    num: "24.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-24.mp3",
+    options: [
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-24-1.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-24-2.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-24-3.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-24-4.webp" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+
+  {
+    text: "[23~26] 듣고 알맞은 그림을 고르십시오.",
+    num: "25.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-25.mp3",
+    options: [
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-26-1.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-26-2.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-26-3.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt01-26-4.webp" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[23~26] 듣고 알맞은 그림을 고르십시오.",
+    num: "26.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-26.mp3",
+    options: [
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-26-1.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-26-2.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-26-3.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-26-4.webp" }
+    ],
+    correct: 3,
+    points: 2.5
+  },
+
+  {
+    text: "[27~30] 듣고 알맞은 대답을 고르십시오.",
+    num: "27.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-27.mp3",
+    options: [
+      { text: "네, 가능합니다." },
+      { text: "버스를 탔습니다." },
+      { text: "한국 사람입니다." },
+      { text: "어제였습니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[27~30] 듣고 알맞은 대답을 고르십시오.",
+    num: "28.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-28.mp3",
+    options: [
+      { text: "관리자에게 보고하세요." },
+      { text: "학교에 갑니다." },
+      { text: "커피를 마십니다." },
+      { text: "친구를 만났습니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[27~30] 듣고 알맞은 대답을 고르십시오.",
+    num: "29.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-29.mp3",
+    options: [
+      { text: "머리가 아파요." },
+      { text: "회사에 다녀요." },
+      { text: "버스를 타요." },
+      { text: "내일이에요." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+  {
+    text: "[27~30] 듣고 알맞은 대답을 고르십시오.",
+    num: "30.",
+    detail: "",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-30.mp3",
+    options: [
+      { text: "버스를 타고 갑니다." },
+      { text: "밥을 먹습니다." },
+      { text: "일을 합니다." },
+      { text: "친구를 만납니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+  {
+    text: "31. 이어지는 말을 고르십시오.",
+    num: "31.",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-31.mp3",
+    options: [
+      { text: "감사합니다." },
+      { text: "병원입니다." },
+      { text: "어제 왔습니다." },
+      { text: "버스를 탔습니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "32. 이어지는 말을 고르십시오.",
+    num: "32.",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-32.mp3",
+    options: [
+      { text: "네, 알겠습니다." },
+      { text: "비가 옵니다." },
+      { text: "한국 사람입니다." },
+      { text: "학교에 갑니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "33. 이어지는 말을 고르십시오.",
+    num: "33.",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-33.mp3",
+    options: [
+      { text: "수고하세요." },
+      { text: "약을 먹었습니다." },
+      { text: "커피를 마십니다." },
+      { text: "내일입니다." }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[34~36] 듣고 알맞은 그림을 고르십시오.",
+    num: "34.",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-34.mp3",
+    options: [
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-34-1.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-34-2.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-34-3.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-34-4.webp" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+
+  {
+    text: "[34~36] 듣고 알맞은 그림을 고르십시오.",
+    num: "35.",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-35.mp3",
+    options: [
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-35-1.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-35-2.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-35-3.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-35-4.webp" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+
+  {
+    text: "[34~36] 듣고 알맞은 그림을 고르십시오.",
+    num: "36.",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-36.mp3",
+    options: [
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-36-1.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-36-2.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-36-3.webp" },
+      { image: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gpt03-36-4.webp" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+
+  {
+    text: "[37~40] 듣고 물음에 답하십시오.",
+    num: "37. 여자는 왜 늦었습니까?",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-37.mp3",
+    options: [
+      { text: "버스가 늦게 와서" },
+      { text: "아파서" },
+      { text: "길을 몰라서" },
+      { text: "늦잠을 자서" }
+    ],
+    correct: 0,
+    points: 2.5
+  },
+
+  {
+    text: "[37~40] 듣고 물음에 답하십시오.",
+    num: "38. 남자는 무엇을 하라고 했습니까?",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-38.mp3",
+    options: [
+      { text: "계속 사용한다" },
+      { text: "관리자에게 알린다" },
+      { text: "집에 간다" },
+      { text: "청소한다" }
+    ],
+    correct: 1,
+    points: 2.5
+  },
+
+  {
+    text: "[37~40] 듣고 물음에 답하십시오.",
+    num: "39. 여자는 무엇을 사려고 합니까?",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-39.mp3",
+    options: [
+      { text: "책" },
+      { text: "신발" },
+      { text: "약" },
+      { text: "우산" }
+    ],
+    correct: 2,
+    points: 2.5
+  },
+
+  {
+    text: "[37~40] 듣고 물음에 답하십시오.",
+    num: "40. 남자는 내일 무엇을 할 예정입니까?",
+    image: "",
+    audio: "https://jettyland.wordpress.com/wp-content/uploads/2026/08/gptz03-40.mp3",
+    options: [
+      { text: "여행" },
+      { text: "회사에 가기" },
+      { text: "병원 방문" },
+      { text: "이사" }
+    ],
+    correct: 1,
+    points: 2.5
+  }
+
+];
